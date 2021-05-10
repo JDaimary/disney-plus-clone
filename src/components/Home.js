5446939f871cbe9835b53d1fab85fc45aaab3600
JDaimary <juliusdaimary5001@gmail.com>
2021-05-10 13:25:57 +0530

    ADD: Viewers

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import "../css/Home.css";
 import ImageSlider from "./ImageSlider";
+import Viewers from "./Viewers";
 
 function Home() {
   return (
     <div className="container">
       <ImageSlider />
+      <Viewers />
     </div>
   );
 }
